f9a15562b273b67b07934107e6ee003d6cb490fa
Anton Golov <jesyspa@gmail.com>
2012-08-23 15:48:14 +0200

    Refactored drawable.
    Now uses function pointers! :D

--- src/systems/event.cpp
+++ src/systems/event.cpp
@@ -1,6 +1,22 @@
 #include "systems/event.hpp"
 #include "world.hpp"
 #include "assert.hpp"
+#include <map>
+
+namespace {
+    typedef void (*EventFunc)(World&, sf::Window&, sf::Event const&);
+
+    void react_Closed(World&, sf::Window& window, sf::Event const&) {
+        window.close();
+    }
+
+    // When we've got all events in here, we can switch to an array.
+#define MAP_EVENTTYPE_TO_REACTION(name) {sf::Event::name, ::react_##name}
+    std::map<sf::Event::EventType, EventFunc> const event_reactions {
+        MAP_EVENTTYPE_TO_REACTION(Closed),
+#undef MAP_EVENTTYPE_TO_REACTION
+    };
+}
 
 EventSystem::EventSystem(World& world, sf::Window* window)
     : System(world), window_(window) {
@@ -11,6 +27,8 @@ void EventSystem::do_update(int) {
     sf::Event event;
     if (!window_->pollEvent(event))
         return;
-    if (event.type == sf::Event::Closed)
-        window_->close();
+    auto reaction = event_reactions.find(event.type);
+    if (reaction != event_reactions.end())
+        (reaction->second)(get_world(), *window_, event);
 }
+
