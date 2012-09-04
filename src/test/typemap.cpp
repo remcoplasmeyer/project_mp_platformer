2eb952cc187490a2a6ec7f3d88d9dd06d7529fe7
Anton Golov <jesyspa@gmail.com>
2012-09-04 14:01:55 +0200

    Largely reimplemented typemap.
    Now does cleanup properly, but is slower.

---
+++ src/test/typemap.cpp
@@ -0,0 +1,46 @@
+#include "typemap.hpp"
+#include <chrono>
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(construct) {
+    TypeMap m;
+}
+
+BOOST_AUTO_TEST_CASE(set_and_retrieve_single) {
+    TypeMap m;
+    m.get<int>() = 5;
+    BOOST_CHECK_EQUAL(m.get<int>(), 5);
+}
+
+BOOST_AUTO_TEST_CASE(set_and_retrieve_multiple) {
+    int const num_maps = 10;
+    TypeMap maps[num_maps];
+    for (int i = 0; i < num_maps; ++i)
+        maps[i].get<int>() = i;
+    for (int i = 0; i < num_maps; ++i)
+        BOOST_CHECK_EQUAL(maps[i].get<int>(), i);
+}
+
+BOOST_AUTO_TEST_CASE(ensure_correct_destruction) {
+    bool destructed = false;
+    struct CheckDestruction {
+        bool* d;
+        ~CheckDestruction() { *d = true; }
+    };
+    {
+        TypeMap m;
+        m.get<CheckDestruction>().d = &destructed;
+    }
+    BOOST_CHECK(destructed);
+}
+
+BOOST_AUTO_TEST_CASE(ensure_reasonable_speed) {
+    auto begin = std::chrono::steady_clock::now();
+    int const num_maps = 10000;
+    TypeMap m[num_maps];
+    for (int i = 0; i < num_maps; ++i)
+        m[i].get<int>() = i;
+    auto end = std::chrono::steady_clock::now();
+    BOOST_CHECK(end - begin < std::chrono::milliseconds(5));
+}
--- src/typemap.hpp
+++ src/typemap.hpp
@@ -1,7 +1,8 @@
 #pragma once
 
+#include "assert.hpp"
 #include <boost/utility.hpp>
-#include <map>
+#include <set>
 
 //! A map of types to values.
 //!
@@ -10,20 +11,151 @@
 class TypeMap : boost::noncopyable {
     template<typename T>
     class TypeMapDetail {
-        static std::map<TypeMap*, T> map_;
+        struct Node : boost::noncopyable {
+            Node(void* key, Node* parent) : key(key), lhs(), rhs(), parent(parent), data() {}
+            void* key;
+            Node* lhs;
+            Node* rhs;
+            Node* parent;
+            T data;
+        };
+        Node* root;
+        
+        typedef void (*destruct_func)(void*);
+        static void destroy_impl(void* p) {
+            delete static_cast<Node*>(p);
+        }
+        destruct_func destroy;
+
+        // No member data past this point.
+
+        Node*& get_parent_to_this(Node* current) {
+            if (!current->parent)
+                return root;
+            if (current->parent->lhs == current)
+                return current->parent->lhs;
+            else
+                return current->parent->rhs;
+        }
+
+        void remove_specific(Node* current) {
+            Node*& parent_to_this = get_parent_to_this(current);
+            if (!current->lhs && !current->rhs) {
+                parent_to_this = nullptr;
+                return;
+            }
+            if (!current->lhs) {
+                parent_to_this = current->rhs;
+                current->rhs->parent = current->parent;
+                return;
+            }
+            if (!current->rhs) {
+                parent_to_this = current->lhs;
+                current->lhs->parent = current->parent;
+                return;
+            }
+            Node* replacement = current->lhs;
+            while (replacement->rhs)
+                replacement = replacement->rhs;
+            replacement->parent->rhs = replacement->lhs;
+            replacement->lhs->parent = replacement->parent;
+
+            parent_to_this = replacement;
+            current->lhs->parent = replacement;
+            current->rhs->parent = replacement;
+            replacement->lhs = current->lhs;
+            replacement->rhs = current->rhs;
+            replacement->parent = current->parent;
+        }
+
       public:
+        static TypeMapDetail map_;
+
+        TypeMapDetail() : destroy(destroy_impl), root() {}
+
+        // Will construct if not found.
+        Node* retrieve(void* key) {
+            if (!root)
+                return root = new Node(key, nullptr);
+            Node* current = root;
+            while (true) {
+                if (key < current->key) {
+                    if (current->lhs)
+                        current = current->lhs;
+                    else
+                        return current->lhs = new Node(key, current);
+                } else if (key > current->key) {
+                    if (current->rhs)
+                        current = current->rhs;
+                    else
+                        return current->rhs = new Node(key, current);
+                } else {
+                    return current;
+                }
+            }
+            ASSERT(!"Unreachable code.");
+        }
+
         static T& get(TypeMap* p) {
-            return map_[p];
+            return map_.retrieve(p)->data;
+        }
+
+        void remove(void* key) {
+            Node* current = root;
+            while (current) {
+                if (key < current->key) {
+                    current = current->lhs;
+                } else if (key > current->key) {
+                    current = current->rhs;
+                } else {
+                    remove_specific(current);
+                    destroy(current);
+                    return;
+                }
+            }
+            ASSERT(!"Tried to remove a non-existent node.");
+        }
+
+        // Kept for debugging.
+        void print(Node* p) const {
+            if (!p)
+                return;
+            std::cerr << "(";
+            print(p->lhs);
+            std::cerr << " " << p->data << " ";
+            print(p->rhs);
+            std::cerr << ")";
+        }
+
+        void print() const {
+            print(root);
+            std::cerr << '\n';
         }
     };
+
+    std::set<void*> members_;
   public:
     //! Retrieve the data associated with the given type.
     template<typename T>
     T& get() {
+        members_.insert(&TypeMapDetail<T>::map_);
         return TypeMapDetail<T>::get(this);
+    }
+
+    ~TypeMap() {
+        struct DummyForCleaningPurposes {};
+        for (auto m : members_) {
+            auto map = static_cast<TypeMapDetail<DummyForCleaningPurposes>*>(m);
+            map->remove(this);
+        }
+    }
+
+    template<typename T>
+    static void print() {
+        TypeMapDetail<T>::map_.print();
     }
 };
  
 template<typename T>
-std::map<TypeMap*, T> TypeMap::TypeMapDetail<T>::map_;
+TypeMap::TypeMapDetail<T> TypeMap::TypeMapDetail<T>::map_;
  
